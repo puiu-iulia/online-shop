a911fe954a54377af62d814dec5034631de4aee8
puiu-iulia <puiu-iulia@users.noreply.github.com>
2020-05-12 19:56:52 +0300

    Added Notes Field

--- constants/ShopWooCommerceAPI.js
+++ constants/ShopWooCommerceAPI.js
@@ -1,10 +1,10 @@
 import WooCommerceAPI from 'react-native-woocommerce-api';
  
 var ShopWooCommerceAPI = new WooCommerceAPI({
-  url: 'https://clients.fizteq.com/gardenia/', // Your store URL
+  url: 'https://gardenia.ro/', // Your store URL
   ssl: true,
-  consumerKey: 'xxx', // Your consumer secret
-  consumerSecret: 'xxx', // Your consumer secret
+  consumerKey: 'ck_5912fabcd383d17a794b9372e661b8dfc0397c22', // Your consumer secret
+  consumerSecret: 'cs_a2132a422d891b4b779c09a07f5f8b64ce94b275', // Your consumer secret
   wpAPI: true, // Enable the WP REST API integration
   version: 'wc/v3', // WooCommerce WP REST API version
   queryStringAuth: true
